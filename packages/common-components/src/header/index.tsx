9b6ea63dd29645699001528186bc1ba0b265a307
leesoyeon93 <soyeon@talktomeinkorean.com>
2023-10-23 19:35:06 +0900

    feat: :sparkles: 공통 패키지 생성

---
+++ packages/common-components/src/header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Colors } from '@common/styles';
+const Header = () => {
+    return <div style={{color : Colors.primaryColor }}>common Header</div>;
+};
+
+export default Header;
---
+++ packages/common-styles/src/colors.ts
@@ -0,0 +1,12 @@
+
+const primaryColor = '#3333FF';
+const errorColor = '#FF604F';
+const LightPurpleColor = '#E0E0FF';
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    primaryColor,
+    errorColor,
+    LightPurpleColor,
+};
