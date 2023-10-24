97a51d26af57ce5a590321c3bec0294622c3aa54
leesoyeon93 <soyeon@talktomeinkorean.com>
2023-10-24 19:06:45 +0900

    feat: :sparkles: 새로운것 7777

--- packages/next1/src/app/page.tsx
+++ packages/next1/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
-      <p>새로운것6666</p>
+      <p>새로운것7777</p>
     </main>
   );
 }
