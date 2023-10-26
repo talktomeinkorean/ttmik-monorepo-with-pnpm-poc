89e13fec387dd1670e02ecffe71120ec8bdc7408
leesoyeon93 <soyeon@talktomeinkorean.com>
2023-10-26 17:08:45 +0900

    feat: :sparkles: header color 변경각???

--- packages/next1/src/app/page.tsx
+++ packages/next1/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
-      <p>나는 슬기님 베프</p>
+      <p>header color 변경각???</p>
     </main>
   );
 }
