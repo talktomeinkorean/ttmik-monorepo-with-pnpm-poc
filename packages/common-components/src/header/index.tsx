8db6dd456c27224d962e704175dbebb232f41595
leesoyeon93 <soyeon@talktomeinkorean.com>
2023-10-26 17:06:14 +0900

    feat: :sparkles: header color 변경

--- packages/common-components/src/header/index.tsx
+++ packages/common-components/src/header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Colors } from '@common/styles';
 const Header = () => {
-    return <div style={{color : Colors.primaryColor }}>common Header</div>;
+    return <div style={{color : Colors.LightPurpleColor }}>common Header</div>;
 };
 
 export default Header;
