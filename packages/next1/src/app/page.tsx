3b9bf86f7db9dc90e9baa07489f05a0aac9d27ee
leesoyeon93 <soyeon@talktomeinkorean.com>
2023-10-26 16:50:07 +0900

    feat: :sparkles: [dev]나는 슬기님 베프

--- packages/next1/src/app/page.tsx
+++ packages/next1/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
-      <p>새로운것11 dev에서 prod으로...</p>
+      <p>나는 슬기님 베프</p>
     </main>
   );
 }
